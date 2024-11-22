52513fd6a06f93a6f31110f32caac12e5504360e
Resham <reshamupadhyay48@gmail.com>
2024-11-22 08:54:25 +0530

    Update script.js

--- week-3/easy/The-Pokémon/script.js
+++ week-3/easy/The-Pokémon/script.js
@@ -1,4 +1,4 @@
-const url1 = 'https://pokeapi.co/api/v2/pokemon/';
+// const url1 = 'https://pokeapi.co/api/v2/pokemon/';
 
 const noCards = document.querySelector("#cards");
 const categories = document.getElementById("categories");
@@ -8,31 +8,26 @@ const cardContainer = document.getElementById("cardContainer");
 const controls = document.querySelector(".controls");
 const container = document.querySelector(".container");
 
+const url = "https://pokeapi.co/api/v2/type/";
 
 function getPokeType() {
-  const url = "https://pokeapi.co/api/v2/type/";
-
-  return fetch(url).then((response) => response.json())
-  .then((data) => {
-    // console.log(data);
-    
-    let pokeType = data.results.map(item => item.name);
-    // console.log(pokeType);
-
-    return pokeType;
-  }).catch((error) => {
-    console.error("Error fetching data: ", error);
-    return [];
-  });
-  
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error fetching data: ", error);
+      return [];
+    });
 }
 
 async function addTypes() {
-  const types = await getPokeType();
-  console.log("Types received in addTypes:", types);
+  const data = await getPokeType();
+  let types = data.results.map((item) => item.name);
 
   if (types && types.length > 0) {
-    types.forEach(type => {
+    types.forEach((type) => {
       const newOption = document.createElement("option");
       newOption.value = type;
       newOption.text = type;
@@ -43,21 +38,45 @@ async function addTypes() {
   }
 }
 
+async function fetchPokemonByType(type) {
+  try {
+    const data = await getPokeType();
+    return data.pokemon
+      .map((p) => parseInt(p.pokemon.url.split("/").slice(-2, -1)[0]))
+      .filter((id) => id <= 898)
+      .sort(() => Math.random() - 0.5);
+  } catch (error) {
+    console.error("Error fetching Pokemon by type:", error);
+    return [];
+  }
+}
+
+function getPokemon() {
+  const limit = parseInt(noCards.value, 10);
+  const finalUrl = `${url}/${categories.value}`;
+  fetch(finalUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const pokemonList = data.pokemon.map((pokeObj) => pokeObj.pokemon);
+      const limitedList = pokemonList.slice(0, limit);
+
+      // render the pokemon
+      console.log(limitedList);
+    });
+}
+
+function formatStatName(statName) {
+  return statName
+    .replace(/-/g, ' ')
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
+function renderPokemonCard(pokemon) {
+  
+}
+
 addTypes();
-
-
-// let getPokeData = () => {    
-//     let id = Math.floor(Math.random() * 100) + 1;    
-//     const finalUrl = url + id;
-    
-//     fetch(finalUrl)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         console.log(data);
-//       });
-//   };
-
-
-  // getPokeData();
-
-  getPokeType();
+getPokeType();
+button.addEventListener("click", getPokemon);
