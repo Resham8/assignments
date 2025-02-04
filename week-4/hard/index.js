adc8357d544c700f25f53d84ded8614d1169517f
Resham <reshamupadhyay48@gmail.com>
2025-02-04 23:23:02 +0530

    Update

--- week-4/hard/index.js
+++ week-4/hard/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const userRoutes = require("./routes/user");
+const todoRoutes = require("./routes/todo");
 dotenv.config();
 
 const app = express();
@@ -10,6 +12,9 @@ app.use(express.json());
 app.get("/healthy", (req, res)=> res.send("I am Healthy"));
 
 //  start writing your routes here
+
+app.use("/user", userRoutes);
+app.use("/todo", todoRoutes);
 
 app.listen(port, ()=> console.log(`server is running at http://localhost:${port}`));
 
