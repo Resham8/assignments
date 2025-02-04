f558c95311fd9d7e361cfc15dc35cb0a4b7e6b10
Resham <reshamupadhyay48@gmail.com>
2025-02-04 09:11:43 +0530

    solving week 4 hard
    added the basic schema and signup and signin endpoints

--- week-4/hard/routes/user.js
+++ week-4/hard/routes/user.js
@@ -1,14 +1,77 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { UserModal, TodoModel} = require("./database/index");
+const { z } = require("zod");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post('/signup',async (req, res) => {
+
+    const requiredBody = z.object({
+        email: z.string().min(3).max(100).email().trim(),
+        name: z.string().min(3).max(100).trim(),
+        password: z.string().min(3).max(30).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/)
+    })
+
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+    if(!parsedDataWithSuccess.success){
+        res.json({
+            error: parsedDataWithSuccess.error
+        })
+    }
+
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password; 
+
+    const hashedPassword = bcrypt.hash(password, 5);
+
+    await UserModal.create({
+        email: email,
+        password:hashedPassword,
+        name:name
+    })
+
+    res.json({
+        msg: "You are signed up"
+    })
+        
 });
 
-router.post('/login', (req, res) => {
-     // Implement user login logic
+router.post('/login',async (req, res) => {
+     const email = req.body.email;
+     const password = req.body.password;
+
+     const response = await UserModal.find({
+        email:email
+     })
+
+     if(!response){
+        res.status(404).json({
+            msg:"User does not exists"
+        })
+     }
+
+     const passwordMatch = await bcrypt.compare(password, response.password);
+
+    if(passwordMatch){
+        const token = jwt.sign({
+            id: response._id.toString()
+        }, JWT_SECRET);        
+
+        res.json({
+            token: token
+        })
+    } else {
+        res.status(403).json({
+            error: "Incorrect Credentials"
+        })
+    }
 });
 
 router.get('/todos', userMiddleware, (req, res) => {
