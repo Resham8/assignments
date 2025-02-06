2a0ba646b60fe643c9cf036d29eb9c663c78e4fc
Resham <reshamupadhyay48@gmail.com>
2025-02-06 23:45:31 +0530

    update

--- week-5/backend/db/index.js
+++ week-5/backend/db/index.js
@@ -1,1 +1,28 @@
-//  start writing from here
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const ObjectId = mongoose.ObjectId;
+
+const mongoConnect = process.env.MONGO_URI;
+mongoose.connect(mongoConnect);
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+
+const TodoSchema = new mongoose.Schema({
+    title:{type:String, required:true},
+    done:Boolean,
+    userId : ObjectId
+});
+
+const User = mongoose.model("User", UserSchema);
+const Todo = mongoose.model("Todo", TodoSchema);
+
+module.exports = {
+    User,
+    Todo
+}
