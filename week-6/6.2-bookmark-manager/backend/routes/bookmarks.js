b26fd3e2742d561f6d730df5bafa6c6af2dc892f
Resham <reshamupadhyay48@gmail.com>
2025-02-12 23:30:25 +0530

    week 6 bookmark

--- week-6/6.2-bookmark-manager/backend/routes/bookmarks.js
+++ week-6/6.2-bookmark-manager/backend/routes/bookmarks.js
@@ -1,13 +1,48 @@
 let bookmarks = []; // in memory space
+let Id = 1;
 
-export async function addBookmark(req,res,next){
-// write here
+export async function addBookmark(req, res, next) {
+  try {
+    const { url, categoy } = req.body;
+    if (!url || !categoy) {
+      res.json({
+        error: "Url and Category required",
+      });
+    }
+
+    const newBookmark = {
+      id: Id++,
+      url: url,
+      categoy: categoy,
+    };
+
+    bookmarks.push(newBookmark);
+
+    res.status(201).json({
+      newBookmark,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-export async function deleteBookmark(req,res,next){
-// write here
+export async function deleteBookmark(req, res, next) {
+  const id = req.params.id;
+
+  const findIndex = bookmarks.findIndex((bookmark) => bookmarks.id == id);
+
+  if (findIndex !== -1) {
+    bookmarks.splice(findIndex, 1);
+    res.status(200).json({
+      msg: "bookmark deleted successfully",
+    });
+  } else {
+    res.status(400).json({
+      error: "bookmark not found",
+    });
+  }
 }
 
-export async function getAllBookmarks(req,res,next){
-// write here
+export async function getAllBookmarks(req, res, next) {
+  res.json(bookmarks);
 }
